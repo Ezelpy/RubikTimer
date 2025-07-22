a54498acfcf5bcbbe526bf5565d2e9b954343695
Ezelpy <enr98.ezequiel@gmail.com>
2025-07-22 19:37:01 -0300

    New icons for buttons, fixed timer movement, ui improvement

--- RubikTimer/rtimer/static/rtimer/js/index.js
+++ RubikTimer/rtimer/static/rtimer/js/index.js
@@ -1,7 +1,13 @@
-const display = document.getElementById("display");
+const minDisplay = document.getElementById("minutes");
+const secDisplay = document.getElementById("seconds");
+const miliDisplay = document.getElementById("milliseconds");
+const secColonDisplay = document.getElementById("secondsColon");
+const minColonDisplay = document.getElementById("minutesColon");
+const controlsContainer = document.getElementById("controls");
 const startButton = document.getElementById("cmdStart");
 const stopButton = document.getElementById("cmdStop");
 const body = document.getElementById("body");
+const TIMER_RESET = "00";
 let timer = null;
 let startTime = 0;
 let elapsedTime = 0;
@@ -10,6 +16,7 @@ let isRunning = false;
 
 function start() {
   controlPress = 0;
+  controlsContainer.style.display = "none";
   if (elapsedTime != 0) {
     reset();
   } 
@@ -17,14 +24,22 @@ function start() {
         body.style.backgroundColor = "lightgreen";
         startTime = Date.now() - elapsedTime;
         // We call the update function every ten miliseconds
-        timer = setInterval(update, 10);
+        timer = setInterval(() => {
+  elapsed = performance.now() - startTime;
+  update(elapsed);
+}, 10); 
         isRunning = true;
    } 
 }
 
 function stop() {
     if (isRunning) {
-        display.style.color = "#279bc2";
+        controlsContainer.style.display = "flex";
+        minDisplay.style.color = "#279bc2";
+        secDisplay.style.color = "#279bc2";
+        miliDisplay.style.color = "#279bc2";
+        minColonDisplay.style.color = "#279bc2";
+        secColonDisplay.style.color = "#279bc2";
         body.style.backgroundColor = "lightblue";  
         clearInterval(timer);
         timer = null;
@@ -42,50 +57,61 @@ function reset() {
     startTime = 0;
     elapsedTime = 0;
     controlPress = 0;
-    display.textContent = "00:00:00";
+    minDisplay.textContent = TIMER_RESET;
+    secDisplay.textContent = TIMER_RESET;
+    miliDisplay.textContent = TIMER_RESET;
     startTime = Date.now();
 }
 
 function update() {
   elapsedTime = Date.now() - startTime;
 
-  const minutes = String(Math.floor(elapsedTime / 60000))
-    .padStart(2, "0");
+  const minutes = String(Math.floor(elapsedTime / 60000)).padStart(2, "0");
+  const seconds = String(Math.floor((elapsedTime % 60000) / 1000)).padStart(2, "0");
+  const milliseconds = String(Math.floor((elapsedTime % 1000) / 10)).padStart(2, "0");
 
-  const seconds = String(
-    Math.floor((elapsedTime % 60000) / 1000)).padStart(2, "0");
+  minDisplay.textContent = minutes;
+  secDisplay.textContent = seconds; 
+  miliDisplay.textContent = milliseconds;
 
-  const centiseconds = String(
-    Math.floor((elapsedTime % 1000) / 10)).padStart(2, "0");
-
-  display.textContent = `${minutes}:${seconds}:${centiseconds}`;
+  
 }
 
+// BUG: counter is not getting reset so i can just press and instantly release
+// the counter many times so that it ends up starting when it gets to 5
 
-
-document.addEventListener('keydown', e => {
-    if (e.code === 'Space' || e.key === ' ') {
+document.addEventListener('keydown', e => { 
+  if (e.code === 'Space' || e.key === ' ') {
       if (!isRunning) {
         controlPress += 1;
-        display.style.color = "red";  
+        minDisplay.style.color = "red";  
+        secDisplay.style.color = "red";  
+        miliDisplay.style.color = "red"; 
+        minColonDisplay.style.color = "red";  
+        secColonDisplay.style.color = "red"; 
         body.style.backgroundColor = "lightsalmon";
       }  
         e.preventDefault();
     }
-
 });
 
 document.addEventListener('keyup', e => {  
   if (e.code === 'Space' || e.key === ' ') {
-        if (!isRunning && controlPress > 15) {
+        if (!isRunning && controlPress > 5) {
             start();
-        } else {
+        } 
+        else {   
             stop();
-               body.style.backgroundColor = "lightblue";                      
+            body.style.backgroundColor = "lightblue";                      
         }
   }
   else if (e.key == 'r') {
     reset();
   }
-  display.style.color = "#279bc2";
+
+  minDisplay.style.color = "#279bc2";  
+  secDisplay.style.color = "#279bc2"; 
+  miliDisplay.style.color = "#279bc2"; 
+  secColonDisplay.style.color = "#279bc2"; 
+  minColonDisplay.style.color = "#279bc2"; 
 });
